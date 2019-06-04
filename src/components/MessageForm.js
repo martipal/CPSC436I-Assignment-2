d2ced4afdf74fa9fe1953463e729cb9117e4e075
Martin Palanca <36322050+martipal@users.noreply.github.com>
2019-06-03 21:23:06 -0700

    Cleaned up code

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import { clearMessages, addMessage } from '../actions';
 
@@ -10,22 +10,22 @@ class MessageForm extends React.Component{
 
     handleSubmit (event){
         event.preventDefault();
-        console.log(event.target.text.value);
         this.props.addMessage(event.target.text.value.toString());
     }
 
     render(){
-        console.log(this.props);
         return (
             <div>
       <form id="form" onSubmit={this.handleSubmit}>
         <label>
+            
           <input id="input_field" ref={input => this.inputField=input} name="text" type="text" defaultValue="Insert Message Here..." onFocus = {() => this.inputField.value=""}/>
         </label>
         <br />
         <input id="add" type="submit" value="Submit" />
         <input type="reset" className="button_stuff" value="Clear Form" />
-      </form><button id="clear_button" className="button_stuff" onClick={() => {this.props.clearMessages(); return false;}}>Clear Message Log
+      </form>
+      <button id="clear_button" className="button_stuff" onClick={() => {this.props.clearMessages(); return false;}}>Clear Message Log
         </button>
     </div>);
     }
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import { createIFrame } from '../actions';
 
@@ -24,9 +24,9 @@ export default class Messages extends React.Component {
         for (var i = 0; i < this.props.numberOfMessages; i++){
             let message = messages[i];
             if (this.props.moveMessages){
-                messageList.push(<li key={i}><marquee behaviour="alternate" onMouseOver={this.stop} onMouseOut={this.start} target="popup" onClick={() => {window.open(("//" + "google.com/search?q=" + message.toString()), 'popup', 'width=400,height=400' ); return false;}} onContextMenu={() => {window.open(("https://www.youtube.com/results?search_query=" + message.toString()), 'popup', 'width=400,height=400'); return false;}}> {message} </marquee></li>);
+                messageList.push(<li key={i}><marquee behaviour="alternate" onClick={() => {window.open(("//" + "google.com/search?q=" + message.toString()), 'popup', 'width=400,height=400' ); return false;}} onContextMenu={() => {window.open(("https://www.youtube.com/results?search_query=" + message.toString()), 'popup', 'width=400,height=400'); return false;}} data-hover="Left click for google, right click for youtube"> {message} </marquee></li>);
             } else {
-                messageList.push(<li key ={i} target="popup" onClick={() => {window.open(("//" + "google.com/search?q=" + message.toString()), 'popup', 'width=400,height=400'); return false;}} onContextMenu={() => {window.open(("https://www.youtube.com/results?search_query=" + message.toString()), 'popup', 'width=400,height=400'); return false;}}> {message} </li>);
+                messageList.push(<li key ={i} onClick={() => {window.open(("//" + "google.com/search?q=" + message.toString()), 'popup', 'width=400,height=400'); return false;}} onContextMenu={() => {window.open(("https://www.youtube.com/results?search_query=" + message.toString()), 'popup', 'width=400,height=400'); return false;}}> {message} </li>);
             }
         }
         
