3f288f316303aa8afc3bfc85ff19abe48b049313
Martin Palanca <36322050+martipal@users.noreply.github.com>
2019-07-13 17:25:46 -0700

    ok

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production'){
 
   app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname,'client','build','index.html'));
-  }
+  });
 }
 
 // catch 404 and forward to error handler
